d3fdf00a6dc2781b57ba76e0208be52841bd91f2
Que Pham <tinhbanbonphuong126@gmail.com>
2018-05-15 09:53:34 +0700

    Link Loop Bunjouchi

--- resources/views/layouts/frontend/partial/component/loop_bunjochi.blade.php
+++ resources/views/layouts/frontend/partial/component/loop_bunjochi.blade.php
@@ -1,4 +1,4 @@
-<a target="_blank" href="@@webRoot/about-us/detail.html">
+<a target="_blank" href="{{ route("bunjouchiDetail", ["id" => $bunjouchi->id]) }}">
     <li class="cell_li">
         <span class="label_rui @if($bunjouchi->status) gray_item @endif">@if($bunjouchi->status)完売御礼@else分譲中@endif</span>
         <span class="description_rui">{{ $bunjouchi->title }}</span>
